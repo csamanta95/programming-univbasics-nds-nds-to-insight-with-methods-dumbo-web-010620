2946d11b473acb6ae44a1397a89c137f92570c18
Samanta Chowdhury <csamanta95@gmail.com>
2020-01-02 21:10:37 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -20,5 +20,11 @@ def gross_for_director(director_data)
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
   result = {}
-  nil
+  director_index = 0
+  while director_index < nds.size do
+    director = nds[director_index]
+    result[director[:name]] = gross_for_director(director)
+    director_index += 1
+  end
+  result
 end
